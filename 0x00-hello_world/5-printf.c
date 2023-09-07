d058acf56f3627dae39908a6d129a65dbf874f9f
theLazyProgrammer <48143641+nanafox@users.noreply.github.com>
2023-09-07 10:08:16 +0000

    task 5 solution - printing with printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - printing with printf
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+
+	return (0);
+}
