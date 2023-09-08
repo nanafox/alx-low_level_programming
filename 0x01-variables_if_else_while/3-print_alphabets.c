f04ead8b3f1b5048876b6b959b2f9bacc2475f45
theLazyProgrammer <48143641+nanafox@users.noreply.github.com>
2023-09-08 08:31:11 +0000

    task 3 solution - lower and uppercase alphabets

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - printing lowercase and uppercase alphabets
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	char c;
+
+	for (c = 'a'; c <= 'z'; c++)
+		putchar(c);
+	for (c = 'A'; c <= 'Z'; c++)
+		putchar(c);
+	putchar('\n');
+
+	return (0);
+}
