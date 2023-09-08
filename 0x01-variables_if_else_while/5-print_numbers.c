237580d4d5a7ad1ae7a00206ec7c8acf915bb090
theLazyProgrammer <48143641+nanafox@users.noreply.github.com>
2023-09-08 08:42:46 +0000

    task 5 solution - numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - printing first ten digits in base 10
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 0; i < 10; i++)
+		printf("%d", i);
+	printf("\n");
+
+	return (0);
+}
