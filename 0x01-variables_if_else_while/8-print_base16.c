aecde82b737a5d0e861fc1972b495518c9e3009c
theLazyProgrammer <48143641+nanafox@users.noreply.github.com>
2023-09-08 08:48:46 +0000

    task 8 solution - hex digits

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - printing the hex range
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	char c;
+
+	for (c = '0'; c <= '9'; c++)
+		putchar(c);
+	for (c = 'a'; c <= 'f'; c++)
+		putchar(c);
+	putchar('\n');
+
+	return (0);
+}
