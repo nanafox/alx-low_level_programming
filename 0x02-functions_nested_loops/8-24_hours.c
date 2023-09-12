1893e18cd99ba2d774b81e1ab6d424a0722eb220
theLazyProgrammer <48143641+nanafox@users.noreply.github.com>
2023-09-12 10:57:21 +0000

    task 8 solution - jack bauer's longest day

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * jack_bauer - jack bauer's longest day.
+ *
+ * Description: prints every minute of the day of Jack Bauer,
+ * starting from 00:00 to 23:59.
+ */
+void jack_bauer(void)
+{
+	char hh, mm;
+
+	for (hh = 0; hh < 24; hh++)
+	{
+		for (mm = 0; mm < 60; mm++)
+		{
+			_putchar((hh / 10) + '0');
+			_putchar((hh % 10) + '0');
+			_putchar(':');
+			_putchar((mm / 10) + '0');
+			_putchar((mm % 10) + '0');
+			_putchar('\n');
+		}
+	}
+}
