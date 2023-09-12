d01157410fcea070390d8c455b9c5ad9824d72b8
theLazyProgrammer <48143641+nanafox@users.noreply.github.com>
2023-09-12 19:32:37 +0000

    task 14 solution first 50 fibonacci numbers

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+
+void print_fibonacci(size_t n);
+
+/**
+ * main - driver code
+ * Return: 0
+ */
+int main(void)
+{
+	print_fibonacci(50);
+	return (0);
+}
+
+/**
+ * print_fibonacci - print the n fibonacci number
+ * @n: range
+ */
+void print_fibonacci(size_t n)
+{
+	size_t fib_x, fib_y, fib_i, i;
+	/* initial value */
+	fib_x = 1;
+	fib_y = 2;
+
+	printf("%ld, %ld, ", fib_x, fib_y);
+
+	for (i = 2, n -= 1; i <= n; i++)
+	{
+		fib_i = fib_x + fib_y; /* get next sequence */
+		printf("%ld%s", fib_i, (i == n) ? "\n" : ", ");
+		/* swap values */
+		fib_x = fib_y;
+		fib_y = fib_i;
+	}
+}
