2ab4c8ce7bff4189df46f5b26c84c3cb237e833e
theLazyProgrammer <48143641+nanafox@users.noreply.github.com>
2023-09-12 08:10:24 +0000

    tas 2 solution - printint 10 times

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - print lowercase alphabets 10 times
+ */
+void print_alphabet_x10(void)
+{
+	int i, j;
+
+	for (i = 0; i < 10; i++)
+	{
+		for (j = 'a'; j <= 'z'; j++)
+			_putchar(j);
+		_putchar('\n');
+	}
+}
