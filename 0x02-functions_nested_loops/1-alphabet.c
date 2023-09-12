5072fdaf0e878735d59c4452d76010072c9e84c2
theLazyProgrammer <48143641+nanafox@users.noreply.github.com>
2023-09-12 07:28:22 +0000

    task 1 solution - alphabets

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * print_alphabet - prints alphabets in lower case
+ */
+void print_alphabet(void)
+{
+	char c;
+
+	for (c = 'a'; c <= 'z'; c++)
+		_putchar(c);
+	_putchar('\n');
+}
