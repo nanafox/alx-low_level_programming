ca36a0793446f901f16c6b21234e53a6a219f0fb
theLazyProgrammer <48143641+nanafox@users.noreply.github.com>
2023-09-12 09:13:38 +0000

    task 4 solution - lowercase or uppercase

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,11 @@
+/**
+ * _isalpha - checks if @c is lowercase or uppercase letter
+ * @c: character
+ * Return: 1 if lowercase or upper, 0 otherwise
+ */
+int _isalpha(int c)
+{
+	if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
+		return (1);
+	return (0);
+}
