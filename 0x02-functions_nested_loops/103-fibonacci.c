a807cdeb638ff464de81b4555978fb37fc10f0e0
theLazyProgrammer <48143641+nanafox@users.noreply.github.com>
2023-09-12 19:50:20 +0000

    task 15 solution - more fibonacci

---
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#define THRESHOLD 4000000
+
+void sum_even_fibonacci(void);
+
+/**
+ * main - driver code
+ * Return: 0
+ */
+int main(void)
+{
+	sum_even_fibonacci();
+	return (0);
+}
+
+/**
+ * sum_even_fibonacci - print the sum of fibonacci numbers
+ */
+void sum_even_fibonacci(void)
+{
+	size_t fib_x, fib_y, fib_i, sum;
+
+	/* initial values */
+	fib_x = 1;
+	sum = fib_y = 2;
+
+	while (!(fib_i > THRESHOLD)) /* loop till condition is met */
+	{
+		fib_i = fib_x + fib_y; /* get next sequence */
+		if (fib_i % 2 == 0)
+			sum += fib_i;
+			/* swap values */
+		fib_x = fib_y;
+		fib_y = fib_i;
+	}
+	printf("%ld\n", sum);
+}
