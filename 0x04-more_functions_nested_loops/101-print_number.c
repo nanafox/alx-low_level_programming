611f5ceedcc78aa60ac6a40153bb9a97b16dfba8
theLazyProgrammer <48143641+nanafox@users.noreply.github.com>
2023-09-14 20:31:33 +0000

    task 12 - update

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -1,5 +1,4 @@
 #include "main.h"
-
 /**
  * print_number - prints numbers using _putchar only
  *
@@ -7,12 +6,14 @@
  */
 void print_number(int n)
 {
+	unsigned int i = n;
+
 	if (n < 0) /* handle negative numbers */
 	{
 		_putchar('-');
-		n = -n;
+		i = -n;
 	}
-	if (n / 10)
-		print_number(n / 10);
-	_putchar((n % 10) + '0');
+	if (i / 10)
+		print_number(i / 10);
+	_putchar((i % 10) + '0');
 }
