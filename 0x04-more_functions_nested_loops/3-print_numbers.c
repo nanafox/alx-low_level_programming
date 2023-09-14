ee70d9a19ce7e97d04e4881b7680a7d30e3656ad
theLazyProgrammer <48143641+nanafox@users.noreply.github.com>
2023-09-14 10:03:31 +0000

    task 3 solution - printing numbers

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * print_numbers - prints numbers from 0 to 9
+ */
+void print_numbers(void)
+{
+	char c;
+
+	for (c = '0'; c <= '9'; c++)
+		_putchar(c);
+	_putchar('\n');
+}
