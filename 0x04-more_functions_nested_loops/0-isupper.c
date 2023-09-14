9068342c0e476638d1bf148a4b82c82b56af1c9e
theLazyProgrammer <48143641+nanafox@users.noreply.github.com>
2023-09-14 09:47:51 +0000

    task 0 solution - isupper

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,10 @@
+/**
+ * _isupper - check if character c is an uppercase letter
+ *
+ * @c: character
+ * Return: 1 if uppercase, 0 otherwise
+ */
+int _isupper(int c)
+{
+	return (c >= 'A' && c <= 'Z');
+}
