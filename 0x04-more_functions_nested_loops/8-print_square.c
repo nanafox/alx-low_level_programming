07311473393c35cb31fc78da3665215a6a415da3
theLazyProgrammer <48143641+nanafox@users.noreply.github.com>
2023-09-14 12:22:10 +0000

    task 8 solution - printing squares

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * print_square - prints square using #
+ * @size: size of square
+ */
+void print_square(int size)
+{
+	int i, j;
+
+	if (size <= 0)
+	{
+		_putchar('\n');
+		return;
+	}
+	for (i = 0; i < size; i++)
+	{
+		for (j = 0; j < size; j++)
+			_putchar('#');
+		_putchar('\n');
+	}
+}
