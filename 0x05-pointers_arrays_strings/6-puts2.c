63d8cd988f46bcee577a46c0f80d531b7cf74a85
theLazyProgrammer <48143641+nanafox@users.noreply.github.com>
2023-09-18 11:23:04 +0000

    printing characters at even positions

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * puts2 - prints characters at even positions
+ * @str: string
+ */
+void puts2(char *str)
+{
+	int i;
+
+	for (i = 0; str[i] != '\0'; i++)
+		if (i % 2 == 0)
+			_putchar(str[i]);
+	_putchar('\n');
+}
