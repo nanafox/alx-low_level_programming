398408498dcc379f675ad8a1187fe27de71a3038
theLazyProgrammer <48143641+nanafox@users.noreply.github.com>
2023-09-20 10:49:31 +0000

    task 3 solution - strcmp

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,17 @@
+/**
+ * _strcmp - compare two strings
+ * @s1: string 1
+ * @s2: string 2
+ *
+ * Return: 0 if @s1 and @s2 are equal, a negative value if @s1 is less than @s2,
+ * a positive value if @s1 is greater than @s2
+ */
+int _strcmp(char *s1, char *s2)
+{
+	for (; *s1 == *s2; s1++, s2++)
+	{
+		if (!*s1)
+			return (0); /* Both strings are equal */
+	}
+	return (*s1 - *s2);
+}
