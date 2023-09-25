b99c4675f78f71ecb9a25ad20ae1715d8aaeddb3
theLazyProgrammer <48143641+nanafox@users.noreply.github.com>
2023-09-25 11:52:07 +0000

    Added a function that searches a string for any set of bytes

---
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,25 @@
+/**
+ * _strpbrk - searches a string for any of a set of bytes
+ * @s: string
+ * @accept: substring
+ *
+ * Description: The _strpbrk() function locates the first occurrence in the
+ * string @s of any of the bytes in the string @accept.
+ *
+ * Return: A pointer to the byte in @s that matches one of the bytes in
+ * @accept, or NULL if no such byte is found.
+ */
+char *_strpbrk(char *s, char *accept)
+{
+	int i, j;
+
+	for (i = 0; *(s + i); i++)
+	{
+		for (j = 0; *(accept + j); j++)
+		{
+			if (*(s + i) == *(accept + j))
+				return (s + i); /* match found*/
+		}
+	}
+	return (0); /* no match found or end of string - return NULL */
+}
