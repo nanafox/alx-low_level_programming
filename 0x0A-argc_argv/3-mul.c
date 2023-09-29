85fa6aac6a4165e03f1d6af70208baf3cf149d10
theLazyProgrammer <48143641+nanafox@users.noreply.github.com>
2023-09-29 11:36:21 +0000

    Performing arithmetic from the CLI - Products

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - Prints the product of two integers
+ * @argc: CLI args counter
+ * @argv: Operands. Expects two operands
+ *
+ * Return: 0
+ */
+int main(int argc, char *argv[])
+{
+	int num1, num2;
+
+	if (argc != 3)
+	{
+		fprintf(stderr, "Error\n");
+		return (1); /* more or less than two arguments received */
+	}
+
+	/* get the integral values of the two operands */
+	num1 = atoi(argv[1]);
+	num2 = atoi(argv[2]);
+
+	printf("%d\n", num1 * num2);
+
+	return (0);
+}
