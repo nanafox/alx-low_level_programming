1ef3777279bfcb14c69a5caec3ee48e0bde2d3b8
theLazyProgrammer <48143641+nanafox@users.noreply.github.com>
2023-09-29 11:11:39 +0000

    Added a function that prints it's name

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - Prints the name of the program
+ * @argc: CLI args counter
+ * @argv: program name
+ *
+ * Return: 0
+ */
+int main(__attribute__((unused)) int argc, char *argv[])
+{
+	puts(argv[0]);
+
+	return (0);
+}
