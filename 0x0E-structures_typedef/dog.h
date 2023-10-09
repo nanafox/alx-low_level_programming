22d01dd1315a22a9f045344f593110c4eb5c9fce
theLazyProgrammer <48143641+nanafox@users.noreply.github.com>
2023-10-09 15:09:51 +0000

    Added all relevant function prototypes, and a typedef

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -15,5 +15,12 @@ struct dog
 };
 
 void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+
+/* set new type definition */
+typedef struct dog dog_t;
+
+dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
 
 #endif
