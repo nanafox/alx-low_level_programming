3d872b5dd35731c97014abb0001b937ce9b3036d
theLazyProgrammer <48143641+nanafox@users.noreply.github.com>
2023-10-09 18:12:46 +0000

    Updated init_dog function to handle memory allocation failure

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -3,16 +3,21 @@
 
 /**
  * init_dog - Initializer for dog structure
- * @d: pointer to dog structure
+ * @new_dog: pointer to dog structure
  * @name: name of dog
  * @age: age of dog
  * @owner: name of dog owner
  */
-void init_dog(struct dog *d, char *name, float age, char *owner)
+void init_dog(struct dog *new_dog, char *name, float age, char *owner)
 {
-	if (d == NULL)
-		d = malloc(sizeof(struct dog)); /* assign enough space to hold data */
-	d->age = age;
-	d->name = name;
-	d->owner = owner;
+	if (new_dog == NULL)
+		new_dog = malloc(sizeof(struct dog)); /* assign enough space to hold data */
+
+	if (new_dog == NULL)
+		return; /* memory allocation failed */
+
+	/* add dog details */
+	new_dog->age = age;
+	new_dog->name = name;
+	new_dog->owner = owner;
 }
