ba8a0d5fbf6277ee82915f8d2103720bf79d96b1
theLazyProgrammer <48143641+nanafox@users.noreply.github.com>
2023-10-09 10:24:43 +0000

    Added a blueprint for a dog entity

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,17 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - blueprint for dog entity
+ * @name: name of dog
+ * @owner: name of dog's owner
+ * @age: dog's age
+ */
+struct dog
+{
+	char *name;
+	char *owner;
+	float age;
+};
+
+#endif
