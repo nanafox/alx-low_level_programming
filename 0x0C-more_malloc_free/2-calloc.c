403842e4754bd7069b1deb8807c1e1e8bae50e22
theLazyProgrammer <48143641+nanafox@users.noreply.github.com>
2023-10-11 08:38:48 +0000

    Updated the _memset function to use unsigned char

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -1,6 +1,6 @@
 #include "main.h"
 
-void *_memset(void *s, int c, unsigned int n);
+void *_memset(void *mem_area, int c, unsigned int size);
 
 /**
  * _calloc - allocate dynamic memory
@@ -16,37 +16,44 @@ void *_memset(void *s, int c, unsigned int n);
  */
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
-	void *mem;
+	void *new_mem;
 
 	if (nmemb == 0 || size == 0)
 		return (NULL);
 
-	mem = malloc(nmemb * size);
-	if (mem == NULL)
+	new_mem = malloc(nmemb * size);
+	if (new_mem == NULL)
 		return (NULL);
 
 	/* initialize memory with zeros*/
-	_memset(mem, 0, nmemb * size);
+	_memset(new_mem, 0, nmemb * size);
 
-	return (mem);
+	return (new_mem);
 }
 
 /**
  * _memset - fill memory with constant byte
- * @s: string buffer
- * @c: byte to write
- * @n: number of bytes to write
+ * @mem_area: the memory area to set
+ * @c: the byte to write
+ * @n: the number of bytes to write
  *
  * Description: The _memset() function fills the first @n bytes of the memory
- * area pointed to by @s with the constant byte @c.
+ * area pointed to by @mem_area with the constant byte @c.
  *
- * Return: a pointer to memory areas @s
+ * Return: a pointer to memory area @mem_area
  */
-void *_memset(void *s, int c, unsigned int n)
+void *_memset(void *mem_area, int c, unsigned int n)
 {
 	unsigned int i;
-	char *tmp = (char *) s;
+	unsigned char *temp_mem_area;
+
+	if (mem_area == NULL || n <= 0)
+		return (mem_area); /* nothing to do here */
+
+	temp_mem_area = (unsigned char *) mem_area;
 
 	for (i = 0; i < n; i++)
-		tmp[i] = c;
+		temp_mem_area[i] = c;
+
+	return (mem_area);
 }
