990a6c3b2e82ddf18e6ec4396e748c5025558106
theLazyProgrammer <48143641+nanafox@users.noreply.github.com>
2023-10-11 14:42:55 +0000

    Added relevant function prototypes

---
+++ 0x0F-function_pointers/function_pointers.h
@@ -0,0 +1,9 @@
+#ifndef FUNC_POINTERS_H
+#define FUNC_POINTERS_H
+
+void print_name(char *name, void (*f)(char *));
+void array_iterator(int *array, size_t size, void (*action)(int));
+int int_index(int *array, int size, int (*cmp)(int));
+
+#endif
+
