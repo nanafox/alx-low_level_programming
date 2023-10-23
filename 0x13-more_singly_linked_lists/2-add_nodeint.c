684b1971fac90f6d02ff02e082e5fbc24d1e6460
theLazyProgrammer <48143641+nanafox@users.noreply.github.com>
2023-10-23 10:05:43 +0000

    Added a function that adds a new node at the beginning of a linked list of integers

---
+++ 0x13-more_singly_linked_lists/2-add_nodeint.c
@@ -0,0 +1,22 @@
+#include "lists.h"
+
+/**
+ * add_nodeint - inserts a node at the beginning of the list
+ * @head: a pointer to the head node
+ * @value: the integer value to store
+ *
+ * Return: a pointer to the new head, else NULL on failure.
+ */
+listint_t *add_nodeint(listint_t **head, const int value)
+{
+	listint_t *new_node = malloc(sizeof(listint_t));
+
+	if (new_node == NULL)
+		return (NULL); /* memory allocation for new node failed */
+
+	new_node->n = value;
+	new_node->next = *head;
+	*head = new_node;
+
+	return (*head);
+}
