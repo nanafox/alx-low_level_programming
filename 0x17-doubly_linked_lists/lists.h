bf93eefe055055a33d9cee526e3d0998178351b0
theLazyProgrammer <48143641+nanafox@users.noreply.github.com>
2023-12-07 09:54:33 +0000

    Added the needed standard header files

--- 0x17-doubly_linked_lists/lists.h
+++ 0x17-doubly_linked_lists/lists.h
@@ -1,6 +1,9 @@
 #ifndef DLISTS_H
 #define DLISTS_H
 
+#include <stdio.h>
+#include <stdlib.h>
+
 /**
  * struct dlistint_s - doubly linked list
  * @n: integer
@@ -8,7 +11,6 @@
  * @next: points to the next node
  *
  * Description: doubly linked list node structure
- *
  */
 typedef struct dlistint_s
 {
