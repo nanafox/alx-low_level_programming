a57d044df862de44b48951961f6f142f580f95e6
theLazyProgrammer <48143641+nanafox@users.noreply.github.com>
2023-12-21 11:33:04 +0000

    Added a function that creates a hash table

---
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -0,0 +1,33 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_create - creates a hash table
+ * @size: the size of the array
+ *
+ * Return: a pointer to the newly created hash table
+ */
+hash_table_t *hash_table_create(unsigned long int size)
+{
+	hash_table_t *hash_map;
+
+	/* check for invalid sizes, e.g. when size is 0 */
+	if (!size)
+		return (NULL);
+
+	/* allocate memory for the entire hash map / table */
+	hash_map = malloc(sizeof(hash_table_t));
+	if (hash_map == NULL)
+		return (NULL); /* memory allocation failed */
+
+	/* allocate memory for the array of linked lists */
+	hash_map->array = malloc(sizeof(char *) * size);
+	if (hash_map->array == NULL)
+	{
+		free(hash_map);
+		hash_map = NULL;
+		return (NULL); /* memory allocation failed for the array */
+	}
+
+	/* return the newly created hash map */
+	return (hash_map);
+}
